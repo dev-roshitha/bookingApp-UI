17c941b7dd701e2204ae7ec6e7b44cdf20d1299a
Ranasinghe98 <sajithroshitha98@gmail.com>
2022-07-05 16:32:37 +0530

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home'
 import List from './pages/list/List'
 import Hotel from './pages/hotel/Hotel'
@@ -7,13 +7,11 @@ import Hotel from './pages/hotel/Hotel'
 const App = () => {
     return(<>
     <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/booking-app-ui-React" element={<Home />} />
-                <Route path="/hotels" element={<List />} />
-                <Route path="/hotels/:id" element={<Hotel />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route exact path="/booking-app-ui-React" element={<Home />} />
+            <Route path="/hotels" element={<List />} />
+            <Route path="/hotels/:id" element={<Hotel />} />
+        </Routes>
     </div>
     </>)
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React  from "react";
 import './navbar.css'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return(<>
         <div className="navbar">
             <div className="navContainer">
+            <NavLink to="/booking-app-ui-React" className="logoLink">
                 <span className="logo">Booking.com</span>
+            </NavLink>
                 <div className="navItems">
                     <button className="navButton">Register</button>
                     <button className="navButton">Login</button>
